f456365e9752bff4740fd1e08189e7e97effea11
jirakiat <75299841+jirakiat@users.noreply.github.com>
2024-09-11 13:11:30 +0700

    Set Url

--- CustermerProject/custerapp/urls.py
+++ CustermerProject/custerapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.add_customer, name='add_customer'),
     path('register/', views.add_customer, name='add_customer'),
     path('customer_list', views.customer_list, name='customer_list'),
     path('edit_customer/<int:customer_id>/', views.edit_customer, name='edit_customer'),
